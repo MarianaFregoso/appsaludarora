8bcc2faaaae61d121b70f0448047bd91945f40be
Maestro <Maestro@MANNY.local>
2018-08-09 13:52:02 -0700

    commit

--- InterfazUsuario/ViewController.swift
+++ InterfazUsuario/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblhola: UILabel!
+    @IBOutlet weak var lblsaludar: UILabel!
+    @IBOutlet weak var lblmeequivoque: UILabel!
+    @IBOutlet weak var txtnombre: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        lblhola.text = "Hola!!!!"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func doTapSaludar(_ sender: Any) {
+        lblsaludar.text = "Hola \(txtnombre.text!)"
+    }
 
 }
 
